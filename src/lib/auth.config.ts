fd2bcf9e19bc5d56f938ad1aef213b808decef3d
dev-arts <socials@dev-arts.de>
2026-03-13 23:21:20 +0100

    fix: deployment

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from 'next-auth'
 
 // Edge-compatible config (no Prisma, no bcrypt)
 export const authConfig: NextAuthConfig = {
+  trustHost: true,
   pages: {
     signIn: '/auth',
   },
